276a290680cc005f55f15d3d28724d20ea1696c3
Ahmed Zaidi <zeydi.ahmed@gmail.com>
2022-03-24 00:50:53 +0100

    creation pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './includes/header/header.component';
 import { FooterComponent } from './includes/footer/footer.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { CocktailListComponent } from './pages/cocktail-list/cocktail-list.component';
+import { CocktailDetailsComponent } from './pages/cocktail-details/cocktail-details.component';
+import { CocktailSearchComponent } from './pages/cocktail-search/cocktail-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CategoriesComponent,
+    CocktailListComponent,
+    CocktailDetailsComponent,
+    CocktailSearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/cocktail-search/cocktail-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cocktail-search',
+  templateUrl: './cocktail-search.component.html',
+  styleUrls: ['./cocktail-search.component.css']
+})
+export class CocktailSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
