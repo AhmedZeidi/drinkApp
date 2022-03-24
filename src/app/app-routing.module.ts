6d133283df29b29c6b3756abe3618f5c09892aa7
Ahmed Zaidi <zeydi.ahmed@gmail.com>
2022-03-24 13:18:39 +0100

    configuration of pages routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { CocktailDetailsComponent } from './pages/cocktail-details/cocktail-details.component';
+import { CocktailListComponent } from './pages/cocktail-list/cocktail-list.component';
+import { CocktailSearchComponent } from './pages/cocktail-search/cocktail-search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'categories', component : CategoriesComponent },
+  { path:'categorie/cocktails', component : CocktailListComponent },
+  { path:'cocktail/detail', component : CocktailDetailsComponent },
+  { path:'conctail/search', component : CocktailSearchComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
