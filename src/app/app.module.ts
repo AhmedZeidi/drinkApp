176b11236c79a3cfba47863eddc53bb46aa68681
Ahmed Zaidi <zeydi.ahmed@gmail.com>
2022-03-26 13:29:51 +0100

    consomation API categories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { CategoriesComponent } from './pages/categories/categories.component';
 import { CocktailListComponent } from './pages/cocktail-list/cocktail-list.component';
 import { CocktailDetailsComponent } from './pages/cocktail-details/cocktail-details.component';
 import { CocktailSearchComponent } from './pages/cocktail-search/cocktail-search.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { CocktailSearchComponent } from './pages/cocktail-search/cocktail-search
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-categories',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
+categories:any = [];
+
   constructor() { }
 
+
   ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories(){
+    axios.get("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
+      .then (response =>{
+        console.log(response.data.drinks);
+        this.categories = response.data.drinks;
+
+
+      })
+      .catch(e =>{
+        console.log(e);
+      })
+
   }
 
 }
