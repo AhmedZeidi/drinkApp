51b45c80b2d04ecd607ab6b44fd83a888f29fa9c
Ahmed Zaidi <zeydi.ahmed@gmail.com>
2022-04-06 12:14:26 +0100

    search component

--- src/app/pages/cocktail-search/cocktail-search.component.ts
+++ src/app/pages/cocktail-search/cocktail-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios, { Axios } from 'axios';
 
 @Component({
   selector: 'app-cocktail-search',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CocktailSearchComponent implements OnInit {
 
+  sCocktail:any = "";
+  cocktails:any = [];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  searchCocktail(search:any){
+    axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`)
+    .then(
+      res =>{
+        this.cocktails = res.data.drinks;
+        console.log(this.cocktails);
+
+      }
+    )
+    .catch()
   }
 
 }
